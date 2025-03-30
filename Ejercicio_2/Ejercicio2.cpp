657b65912e0034901fc0f5a43fbadf504099f6d2
miafd <mfrancinidorrego@udesa.edu.ar>
2025-03-30 10:58:56 -0300

    Implementación de la clase Estudiante con métodos para agregar cursos y calcular el promedio general

--- Ejercicio_2/Ejercicio2.cpp
+++ Ejercicio_2/Ejercicio2.cpp
@@ -1,1 +1,44 @@
+#include <iostream>  // Para entrada y salida estándar
+#include <vector> // Para manejar vectores
+#include <string>
+#include <algorithm> // Para manejar sort
+
+using namespace std;
+
+class Estudiante {
+    private:
+    string nombre_completo;
+    int legajo;
+    vector<pair<string, double>> cursos; // Nombre del curso y nota final
+    public:
+    // Constructor
+    Estudiante(string nombre, int legajo) : nombre_completo(nombre), legajo(legajo) {}
+
+    // Métodos
+    void agregar_curso(string nombre_curso, double nota_final); //Agregar curso y nota
+
+    //Getters
+    string get_nombre_completo() { return nombre_completo; }
+    int get_legajo() { return legajo; }
+    double get_promedio_general() {
+        if (cursos.empty()) return 0.0;
+        double suma = 0;
+        for (const auto& curso : cursos) {
+            suma += curso.second;
+        }
+        return suma / cursos.size();
+    }
+};
+
+class Curso{};
+
+void Estudiante::agregar_curso(string nombre_curso, double nota_final) {
+    if (nota_final < 0 || nota_final > 10) {
+        cout << "Error: La nota debe estar entre 0 y 10." << endl;
+        return;
+    }
+    cursos.push_back({nombre_curso, nota_final});
+}
+
+
 
